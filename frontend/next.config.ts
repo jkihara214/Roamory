f0b9837ed85a6bdaa3d5830239bb89990b48fd71
jkihara214 <jkihara214@gmail.com>
2025-09-23 22:45:05 +0900

    [039]Fix:static_assets

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   trailingSlash: true,
+  // 静的エクスポート時にアセットパスを相対パスにする
+  assetPrefix: "./",
 };
 
 export default nextConfig;
